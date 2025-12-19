fe4b19947ba7aa5d099c7849fd0d75a2651d010e
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-12-19 16:41:01 +0000

    use injected UdpIO interface in echo module

--- src/echo.rs
+++ src/echo.rs
@@ -1,20 +1,19 @@
 use embassy_net::Stack;
 use embassy_net::udp::{PacketMetadata, UdpSocket};
+use w5500_json::handle_packet;
 
 pub async fn run(stack: Stack<'static>) -> ! {
     let mut rx_buf = [0; 4096];
     let mut tx_buf = [0; 4096];
     let mut rx_meta = [PacketMetadata::EMPTY; 16];
     let mut tx_meta = [PacketMetadata::EMPTY; 16];
-    let mut buf = [0; 4096];
+    let mut buffer = [0; 4096];
 
     let mut socket = UdpSocket::new(stack, &mut rx_meta, &mut rx_buf, &mut tx_meta, &mut tx_buf);
 
     socket.bind(1234).unwrap();
 
     loop {
-        if let Ok((n, ep)) = socket.recv_from(&mut buf).await {
-            let _ = socket.send_to(&buf[..n], ep).await;
-        }
+        handle_packet(&socket, &mut buffer).await;
     }
 }
